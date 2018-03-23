f8c36e342cd8ebe338c9a904b31c2f8d48e4e2e5
郑杰 <zhejie@rsclouds.com>
2018-03-23 17:34:02 +0800

    mm

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,7 +1,45 @@
 <template>
        <div class="accounts">
            <!--账户-->
-           账户
+         <el-row>
+           <el-col :span="4"><el-button type="primary">添加按钮</el-button></el-col>
+         </el-row>
+
+         <el-table
+           :data="accountsTable"
+           stripe
+           style="width: 100%">
+           <el-table-column
+             prop="name"
+             label="名称"
+             width="180">
+           </el-table-column>
+           <el-table-column
+             prop="rolename"
+             label="角色"
+             width="180">
+           </el-table-column>
+           <el-table-column
+             prop="roletype"
+             label="RoleType">
+           </el-table-column>
+           <el-table-column
+             prop="domain"
+             label="域">
+           </el-table-column>
+           <el-table-column
+             prop="state"
+             label="状态">
+           </el-table-column>
+           <el-table-column
+             fixed="right"
+             label="快速查看"
+             width="100">
+             <template slot-scope="scope">
+               <el-button @click="handleClick(scope.row.id)" type="text" size="small">查看</el-button>
+             </template>
+           </el-table-column>
+         </el-table>
        </div>
 </template>
 
@@ -10,10 +48,41 @@ export default {
   name: 'v-accounts',
   data () {
     return {
+      accountsParam:{
+        command:'listAccounts',
+        response:'json',
+        listAll:'true',
+        page:'1',
+        pagesize:'20',
+        _:'1521791056413',
+      },
+      accountsTable:[],
+      accountsCount:0,
     }
   },
   methods:{
+    getAccounts(){
+      this.$http.get('client/api',{params:this.accountsParam}).then(function(response){
+        this.accountsTable = response.listaccountsresponse.account;
+        this.accountsCount = response.listaccountsresponse.count;
+      }.bind(this)).catch(function(error){
+        //报错信息提示
+        console.log(error.response.data)
+        this.$message({
+          showClose: true,
+          message: error.response.data,
+          type: 'error'
+        });
+      }.bind(this))
+      console.log( this.accountsTable)
+    },
+    handleClick(id) {
+      console.log(id);
+    }
   },
+  mounted(){
+    this.getAccounts()
+  }
 }
 </script>
 
--- src/components/Instances.vue
+++ src/components/Instances.vue
@@ -1,7 +1,16 @@
 <template>
        <div class="instances">
            <!--实例-->
-           实例
+         <ul>
+           <template v-for="item in instancesTable">
+             <li>
+               <el-container>
+                 <el-header>{{item.name}}</el-header>
+                 <el-main>{{item.name}}</el-main>
+               </el-container>
+             </li>
+           </template>
+         </ul>
        </div>
 </template>
 
@@ -10,13 +19,52 @@ export default {
   name: 'v-instances',
   data () {
     return {
+      instancesParam:{
+        command:'listVirtualMachines',
+        response:'json',
+        listAll:'true',
+        page:'1',
+        pagesize:'20',
+        _:'1521796845746',
+      },
+      instancesTable:[],
+      instancesCount:0,
     }
   },
   methods:{
+    getInstances(){
+      this.$http.get('client/api',{params:this.instancesParam}).then(function(response){
+        this.instancesTable = response.listvirtualmachinesresponse.virtualmachine;
+        this.instancesCount = response.listvirtualmachinesresponse.count;
+      }.bind(this)).catch(function(error){
+        //报错信息提示
+        console.log(error.response.data)
+        this.$message({
+          showClose: true,
+          message: error.response.data,
+          type: 'error'
+        });
+      }.bind(this))
+      console.log( this.instancesTable)
+    },
+    handleClick(id) {
+      console.log(id);
+    }
   },
+  mounted(){
+    this.getInstances()
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" type="text/css" scoped>
+  li{
+    width: 20%;
+    background: #eee;
+    border-radius: 5px;
+    float: left;
+    margin-right: 5%;
+    list-style: none
+  }
 </style>
